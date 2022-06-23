c683a9afdbf8c11d3e3e8d73baef97b112a3ed8d
Vinícius Climaco <69286506+viniciusclimaco@users.noreply.github.com>
2022-06-23 07:20:49 -0300

    Entrega da Solution Sprint

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps v4"
+    return "Laboratório Pipeline DevOps v4 - Solution Strint"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
